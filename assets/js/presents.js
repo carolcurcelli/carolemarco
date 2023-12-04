d7dfc73c359d56289940af56c33e1fc7f83a8e33
carolcurcelli <carolsvntos@gmail.com>
2023-12-04 14:34:18 -0300

    update presentes

--- assets/js/presents.js
+++ assets/js/presents.js
@@ -10,18 +10,18 @@
 
             for(let i = 0; i < count; i++) {
                 presentArea[0].innerHTML += `
-                    <li class="gift ${json[i].stock == 0 ? '--bought' : ''}">
+                    <li class="gift">
                         <h3 class="gift__title">${json[i].name}</h3>
-                        <div class="gift__image" style="background-image: url('${json[i].image}')"></div>
+                        <div class="gift__image" style="background-image: url('../assets/img/presents/${json[i].image}')"></div>
+                        <span class="gift__date"><strong>${json[i].date}</strong></span>
+                        <span class="gift__quantity">
+                            Obrigado ${json[i].gifter}!
+                        </span>
                         <p>
                             ${json[i].description}
                         </p>
                         <div class="gift__wrapper">
-                            <span class="gift__value">R$ ${json[i].value}</span>
-                            <span class="gift__quantity">
-                                Resta${json[i].stock == 1 ? '' : 'm'} ${json[i].stock}
-                            </span>
-                            <a href="https://api.whatsapp.com/send?phone=5511971210335&text=Oi%20Marco,%20vim%20mandar%20o%20comprovante%20do%20presente%20do%20casamento!" target="_blank" class="button">Enviar comprovante</a>
+                            <a href="/paris#${json[i].name.toLowerCase().split(' ').join('').replace(/[^a-z0-9]/gi, '')}" class="button">Ver mais</a>
                         </div>
                     </li>
                 `;
@@ -35,18 +35,18 @@
 
             for(let i = 0; i < count; i++) {
                 presentArea[1].innerHTML += `
-                    <li class="gift ${json[i].stock == 0 ? '--bought' : ''}">
+                    <li class="gift">
                         <h3 class="gift__title">${json[i].name}</h3>
                         <div class="gift__image" style="background-image: url('${json[i].image}')"></div>
+                        <span class="gift__date"><strong>${json[i].date}</strong></span>
+                        <span class="gift__quantity">
+                            Obrigado ${json[i].gifter}!
+                        </span>
                         <p>
                             ${json[i].description}
                         </p>
                         <div class="gift__wrapper">
-                            <span class="gift__value">R$ ${json[i].value}</span>
-                            <span class="gift__quantity">
-                                Resta${json[i].stock == 1 ? '' : 'm'} ${json[i].stock}
-                            </span>
-                            <a href="https://api.whatsapp.com/send?phone=5511971210335&text=Oi%20Marco,%20vim%20mandar%20o%20comprovante%20do%20presente%20do%20casamento!" target="_blank" class="button">Enviar comprovante</a>
+                            <a href="/paris#${json[i].name.toLowerCase().split(' ').join('').replace(/[^a-z0-9]/gi, '')}" class="button">Ver mais</a>
                         </div>
                     </li>
                 `;
